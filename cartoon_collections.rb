13560ceb4a182235619a262e0fa8e19ef5aea4f0
Alison Shin <alisonyshin@gmail.com>
2018-07-31 03:36:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,6 @@ def my_find(collection)
   end
 end
 
-collection = (1..100).to_a
-my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
-
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(soup)
 
